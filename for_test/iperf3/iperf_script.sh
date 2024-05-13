8b851157a56a2b0cf481415976331dbe3f116432
Ubuntu <cloudadmin@node1.cluster.local>
2024-05-13 22:40:23 +0300

    Fix script iperf3

--- for_test/iperf3/iperf_script.sh
+++ for_test/iperf3/iperf_script.sh
@@ -3,10 +3,10 @@ set -x
 kubectl apply -f /home/cloudadmin/for_diploma/for_test/iperf3/Podserver.yaml
 sleep 2
 iperf_server_ip=$(kubectl get po -o wide | awk '{print $6}'| tail -n 1)
-sed -i -E 's/\b([0-9]{1,3}\.){3}[0-9]{1,3}\b/$iperf_server_ip/g'/home/cloudadmin/for_diploma/for_test/iperf3/Podclient.yaml
-for i in {1..10}
-do
-    kubectl apply -f /home/cloudadmin/for_diploma/for_test/iperf3/Podclient.yaml
-    sleep 5
-    kubectl delete -f /home/cloudadmin/for_diploma/for_test/iperf3/Podclient.yaml
-done
+sed -i -E "s/\b([0-9]{1,3}\.){3}[0-9]{1,3}\b/$iperf_server_ip/g" /home/cloudadmin/for_diploma/for_test/iperf3/Podclient.yaml
+#for i in {1..3}
+#do
+#    kubectl apply -f /home/cloudadmin/for_diploma/for_test/iperf3/Podclient.yaml
+#    sleep 5
+#    kubectl delete -f /home/cloudadmin/for_diploma/for_test/iperf3/Podclient.yaml
+#done
