2041efb9cbbd0635119eb86c6bda5e3f3ad8812e
Ubuntu <cloudadmin@node1.cluster.local>
2024-05-17 13:39:37 +0300

    Add test iperf for lowlevel runtime

---
+++ for_test/iperf3/kata/iperf_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+kubectl apply -f ~/for_diploma/for_test/iperf3/kata/Podserver.yaml
+sleep 80
+iperf_server_ip=$(kubectl get po iperf-server -o wide | awk '{print $6}'| tail -n 1)
+sed -i -E "s/\b([0-9]{1,3}\.){3}[0-9]{1,3}\b/$iperf_server_ip/g" ~/for_diploma/for_test/iperf3/kata/Podclient.yaml
+for i in {1..3}
+do
+    kubectl apply -f ~/for_diploma/for_test/iperf3/kata/Podclient.yaml
+    sleep 25
+    kubectl delete -f ~/for_diploma/for_test/iperf3/kata/Podclient.yaml
+done
---
+++ for_test/iperf3/runc/iperf_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+kubectl apply -f ~/for_diploma/for_test/iperf3/runc/Podserver.yaml
+sleep 10
+iperf_server_ip=$(kubectl get po -o wide | awk '{print $6}'| tail -n 1)
+sed -i -E "s/\b([0-9]{1,3}\.){3}[0-9]{1,3}\b/$iperf_server_ip/g" ~/for_diploma/for_test/iperf3/runc/Podclient.yaml
+for i in {1..3}
+do
+    kubectl apply -f ~/for_diploma/for_test/iperf3/runc/Podclient.yaml
+    sleep 5
+    kubectl delete -f ~/for_diploma/for_test/iperf3/runc/Podclient.yaml
+done
